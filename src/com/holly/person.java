1443a7c14a5709ed0aa4b9bbc6348839ea86d350
holly deng <holly199975@gmail.com>
2017-10-17 16:32:37 +0800

    variable, primitive

--- src/com/holly/person.java
+++ src/com/holly/person.java
@@ -1,5 +1,12 @@
 package com.holly;
 
 public class person {
-
+ public void sayhello(){
+	 System.out.println("Hello");
+ }
+	 public static void main(String[] args) {
+		 Person p = new Person();
+		 p
+		}
+ 
 }
