6c055cf26dc0655eb156e9ac6b6dd8da8316f36e
Holly Deng <holly199975@gmail.com>
2017-11-03 23:28:03 +0800

    迴圈修改

--- src/com/holly/Niner.java
+++ src/com/holly/Niner.java
@@ -1,18 +1,7 @@
 package com.holly;
 
 public class Niner {
-	/*
-	 	2 x 1 =  2
-	 	2 x 2 =  4
-	 	2 x 3 =  6
-	 	2 x 4 =  8
-	 	2 x 5 = 10
-	 	2 x 6 = 12
-	 	2 x 7 = 14
-	 	2 x 8 = 16
-	 	2 x 9 = 18
-	 */
-
+	/*九九乘法表*/
 	public static void main(String[] args) {
 		for(int n = 2 ; n <= 9 ; n++) {
 			for(int i = 1; i <= 9 ; i++){
@@ -23,6 +12,17 @@ public class Niner {
 				}
 			}System.out.println();
 		}
+		/*
+	 	2 x 1 =  2
+	 	2 x 2 =  4
+	 	2 x 3 =  6
+	 	2 x 4 =  8
+	 	2 x 5 = 10
+	 	2 x 6 = 12
+	 	2 x 7 = 14
+	 	2 x 8 = 16
+	 	2 x 9 = 18
+	 */
 		/*for(int i = 1; i <= 9 ; i++){
 			if((2*i) < 10){
 			System.out.println("2 x "+ i + " =  " + (2*i));
