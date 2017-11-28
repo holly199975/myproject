bd8803ba72284077c2c0a37d301f3942de970161
Holly Deng <holly199975@gmail.com>
2017-11-28 14:40:02 +0800

    陣列

---
+++ src/com/holly/Dice.java
@@ -0,0 +1,17 @@
+package com.holly;
+
+public class Dice {
+
+	public static void main(String[] args) {
+		int n[] = {4,5,6 };
+				//new int[3];
+		/*n[0] = 4;
+		n[1] = 5;
+		n[2] = 6;*/
+		for(int i=0; i<3; i++){
+			System.out.println(n[i]);
+		}
+		
+	}
+
+}
