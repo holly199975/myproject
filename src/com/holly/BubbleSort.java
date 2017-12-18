917a7ee823a7b7b32e7cdf35b2a41006ba36711b
Holly Deng <holly199975@gmail.com>
2017-12-18 23:40:28 +0800

    bubble sort 未改完

--- src/com/holly/BubbleSort.java
+++ src/com/holly/BubbleSort.java
@@ -1,9 +1,15 @@
 package com.holly;
 
+import java.util.Scanner;
+
 public class BubbleSort {
 	//排序 51,23,6,65,15
 	public static void main(String[] args) {
-	int n[] = {	51,23,6,65,15};
+		/*System.out.print("請輸入5個數字:");
+		Scanner scanner = new Scanner(System.in);
+		String line = scanner.nextLine();
+		n= Integer.parseInt(line);*/
+	int n[] = {51,23,6,65,15};
 	for(int i=0; i<n.length - 1; i++){
 		for(int j=(i+1); j<n.length; j++){ 
 			if(n[i] > n[j]){
