80f19d1b0454c72cff3566e9a6ed839be7f4f62a
Holly Deng <holly199975@gmail.com>
2017-12-26 00:18:16 +0800

    販賣機(未完成)

---
+++ src/com/ibm/VendorMachine.java
@@ -0,0 +1,59 @@
+package com.ibm;
+
+import java.util.Scanner;
+
+public class VendorMachine {
+
+	public static void main(String[] args) {
+		int i = 1;
+		int m = 0;
+		while(i>0) {
+		System.out.println("請投幣或選擇飲料(a或b或c),或輸入0結束:");
+		Scanner scanner = new Scanner(System.in);
+		String line = scanner.nextLine();
+		
+		switch(line) {
+		case "1" :
+			 m = m +1;
+			System.out.println("目前餘額:"+m);
+			break;
+		case "5" :
+			 m = m +5;
+			System.out.println("目前餘額:"+m);
+			break;
+		case "10" :
+			 m = m +10;
+			System.out.println("目前餘額:"+m);
+			break;
+		case "a" :
+			 if( (m -15)>=0) {
+			System.out.println("DON!");	 
+			System.out.println("目前餘額:"+(m-15));
+			 }else
+				 System.out.println("BEEP!"+"目前餘額:"+m);
+			 break;
+		case "b" :
+			if( (m -20)>=0) {
+				System.out.println("DON!");	 
+				System.out.println("目前餘額:"+(m-20));
+				 }else
+					 System.out.println("BEEP!"+"目前餘額:"+m);
+				 break;
+		case "c" :
+			if( (m -30)>=0) {
+				System.out.println("DON!");	 
+				System.out.println("目前餘額:"+(m-30));
+				 }else
+					 System.out.println("BEEP!"+"目前餘額:"+m);
+				 break;
+		case "0" :
+			System.out.println("Bye!");
+			i = -1;
+			break;
+		}
+		}
+	 
+		
+	}
+
+}
