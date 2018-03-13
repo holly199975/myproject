86b095bb61351ba2f2ffbfe374f7e56a5dcc95fc
Holly Deng <holly199975@gmail.com>
2018-03-13 16:23:08 +0800

    Menber

--- src/com/java2/object/Member.java
+++ src/com/java2/object/Member.java
@@ -3,11 +3,14 @@ package com.java2.object;
 public class Member {
 	int amount;
 	float discount = 0.05f;
+	private int gift;
 	public Member(int amount){
 		this.amount = amount;
 	}
+	
+	
 	public void print(){
-		 int total = (int)(amount*discount);
+		 int total = (int)(amount*discount) + gift;
 		System.out.println("當月消費金額:"+amount +"\t" +"折價券" +total);
 	}
 	
