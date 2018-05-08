8238d2a9e485b58e5f52a7a09467c7b222b1800f
Holly Deng <holly199975@gmail.com>
2018-05-08 15:23:23 +0800

    Stepik

--- src/com/java2/object/StepikTester2.java
+++ src/com/java2/object/StepikTester2.java
@@ -7,18 +7,38 @@ import java.util.List;
 public class StepikTester2 {
 
 	public static void main(String[] args) {
-		List<Integer> list = new ArrayList<>();
-		list.add(2);
-		list.add(0);
-		list.add(1);
-		list.add(7);
-		
-		System.out.println(list);
-		
-		List<String> nameList = new ArrayList<>(Arrays.asList("Mr.Green", "Mr.Yellow", "Mr.Red"));
-		for (String name : nameList) {
-		    System.out.println(name);
-		}
-	}
+
+	    List<GreekLetter> letterList = new ArrayList<>();
+	    letterList.add(new GreekLetter("Gamma",  3));
+	    letterList.add(new GreekLetter("Omega", 24));
+	    letterList.add(new GreekLetter("Alpha",  1));
+
+
+	    Object[] objArray = letterList.toArray();
+
+
+	    for (int i = 0; i < objArray.length; i++) {
+	      System.out.println(objArray[i]);
+	    }
+	  }
+
+	  static class GreekLetter {
+
+	    private String letter;
+	    private Integer position;
+
+	    public GreekLetter(String letter, Integer position) {
+	      this.letter = letter;
+	      this.position = position;
+	    }
+
+	    @Override
+	    public String toString() {
+	      return "{" +
+	              "letter='" + letter + '\'' +
+	              ", position=" + position +
+	              '}';
+	    }
+	  }
 
 }
