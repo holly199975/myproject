b54636820876d84261adfd5e5a452f1d672e8465
Holly Deng <holly199975@gmail.com>
2018-05-11 23:43:09 +0800

    Stepik Class

--- src/com/java2/object/StepikTester.java
+++ src/com/java2/object/StepikTester.java
@@ -2,32 +2,66 @@ package com.java2.object;
 
 public class StepikTester {
 	
-	class Employee {
+	 class Employee {
 		String name;
 		String email;
 		int experience;
-		
-		public void getName(){
+		public Employee(String name,String email, int experience) {
+			super();
+			this.name = name;
+			this.email = email;
+			this.experience = experience;
 			
 		}
+		
+		public String getName(){
+			return name;
+		}
+		public String getEmail() {
+			return email;
+		}
+		public int getExperience() {
+			return experience;
+		}
 		}
 
-		class Developer extends Employee {
+		 class Developer extends Employee {
+			String mainLanguage;
+			String[] skills;
+			public Developer(String name, String email, int experience,String mainLanguage,String[] skills) {
+				super(name, email, experience);
+				this.mainLanguage = mainLanguage;
+				this.skills = skills;
+				
+			}
+			
+			public String getMainLanguage() {
+				return mainLanguage;
+			}
+			public String[] getSkills() {
+				return skills;
+			}
+			
+				
+			
 
-		  // write fields
-
-		  // write constructor
-
-		  // write getters
 		}
 
-		class DataAnalyst extends Employee {
-
-		  // write fields
-
-		  // write constructor
-
-		  // write getters
+		 class DataAnalyst extends Employee {
+			boolean phd;
+			String[] methods;
+			public DataAnalyst(String name, String email, int experience,boolean phd,String[] methods) {
+				super(name, email, experience);
+				this.phd = phd;
+				this.methods = methods;
+			}
+			public boolean isPhD() {
+				return phd;
+			}
+			public String[] getMethods() {
+				return methods;
+			}
+			
 		}
 
 	public static void main(String[] args) {
@@ -36,6 +70,7 @@ public class StepikTester {
 
 		String[] mlMethods = { "neural networks", "decision tree", "bayesian algorithms" };
 		DataAnalyst analyst = new DataAnalyst("John", "john@gmail.com", 2, true, mlMethods);
+		
 	}
 
 }
