7161519d7e0dda5b2027b2aa561cbaa676910901
Holly Deng <holly199975@gmail.com>
2018-06-07 00:25:12 +0800

    VendingMain

--- src/com/java2/oo/VendingMachine.java
+++ src/com/java2/oo/VendingMachine.java
@@ -6,4 +6,7 @@ import java.util.List;
 public class VendingMachine {
 	List<Drink> drinks = new ArrayList<>();
 	
+	public void start() {
+		
+	}
 }
--- src/com/java2/oo/VendingMain.java
+++ src/com/java2/oo/VendingMain.java
@@ -1,20 +1,37 @@
 package com.java2.oo;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 
 public class VendingMain {
 
 	public static void main(String[] args) {
 		VendingMachine vm = new VendingMachine();
-		FileReader fr = new FileReader("Vending.txt");
-		BufferedReader in = new BufferedReader(fr);
-		String line = in.readLine();
-		String[] tokens = line.split(",");
-		int count = Integer.parseInt(tokens[0]);
+		FileReader fr;
+		try {
+			fr = new FileReader("Vending.txt");
+			BufferedReader in = new BufferedReader(fr);
+			String line = in.readLine();
+			String[] tokens = line.split(",");
+			int count = Integer.parseInt(tokens[0]);
+			for(int i=0;  i<count; i++) {
+				String name = tokens[i*2+1];
+				int price = i*2+2;
+				vm.drinks.add(new Drink(i+1, name, price));
+			}
+			vm.start();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 		
-		vm.drinks.add(new Drink(id, name, price));
+		
 	}
 
 }
