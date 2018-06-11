05498869214b9d394eec1d484d7551b0d4d69b74
Holly Deng <holly199975@gmail.com>
2018-06-11 23:56:01 +0800

    maze

--- src/com/java2/oo/MazeMain.java
+++ src/com/java2/oo/MazeMain.java
@@ -1,7 +1,14 @@
 package com.java2.oo;
 
 public class MazeMain {
-
+	/*有一遊戲場地，長寬由執行當時決定，場地中亂數產生陷阱，陷阱樹也由當時才決定
+	 * 
+	 * 有一個玩家在這場地中，初始hp值為100，走一步消耗1hp，撞到邊界扣5hp，掉到陷阱扣20hp
+	 * 
+	 * 有個檔案(maze.txt)資訊如下:
+	 * 6,4
+	 * 5
+	 * 6,8,2,4,2,2,6,6,8,4,2,2,6*/
 	public static void main(String[] args) {
 		
 		
