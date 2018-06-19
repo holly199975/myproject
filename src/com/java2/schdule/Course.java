d892d1feffd41c2389aead91484120de741c43f4
Holly Deng <holly199975@gmail.com>
2018-06-19 23:10:23 +0800

    Course

--- src/com/java2/schdule/Course.java
+++ src/com/java2/schdule/Course.java
@@ -9,5 +9,40 @@ public class Course {
 	
 	public Course(String id,String name,int weekDay,int hour,int duration){
 		this.id = id;
+		this.name = name;
+		this.weekDay = weekDay;
+		this.hour = hour;
+		this.duration = duration;
+	}
+	
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setWeekDay(int weekDay) {
+		this.weekDay = weekDay;
+	}
+	public int getWeekDay() {
+		return weekDay;
+	}
+	public void setHour(int hour) {
+		this.hour = hour;
+	}
+	public int hour() {
+		return hour;
+	}
+	public void setDuration(int duration) {
+		this.duration = duration;
+	}
+	public int getDuration() {
+		return duration;
 	}
 }
