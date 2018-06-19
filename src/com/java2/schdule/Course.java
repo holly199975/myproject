b95273a86f22d1d32cd4d497ba2cc3602f305b20
Holly Deng <holly199975@gmail.com>
2018-06-19 16:30:46 +0800

    schedual

---
+++ src/com/java2/schdule/Course.java
@@ -0,0 +1,13 @@
+package com.java2.schdule;
+
+public class Course {
+	String id;
+	String name;
+	int weekDay;
+	int hour;
+	int duration;
+	
+	public Course(String id,String name,int weekDay,int hour,int duration){
+		this.id = id;
+	}
+}
--- src/com/java2/schdule/Main.java
+++ src/com/java2/schdule/Main.java
@@ -1,15 +1,36 @@
 package com.java2.schdule;
 
+import java.nio.channels.ReadableByteChannel;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+/* 有一文字檔儲存了課表資料(schedule.txt)
+ * 請讀取資料後，設計程式可檢查是否有空檔?
+ * 如:
+ * 請輸入星期(1-7):2
+ * 請輸入時(0-24):11
+ * 【有空檔】
+ * 請輸入星期(1-7):4
+ * 請輸入時(0-24):14
+ * 【有課: C004 English 到 16時 】*/
 public class Main {
-	/* 有一文字檔儲存了課表資料(schedule.txt)
-	 * 請讀取資料後，設計程式可檢查是否有空檔?
-	 * 如:
-	 * 請輸入星期(1-7):2
-	 * 請輸入時(0-24):11
-	 * 【有空檔】
-	 * 請輸入星期(1-7):4
-	 * 請輸入時(0-24):14
-	 * 【有課: C004 English 到 16時 】*/
+	List<Course> courses = new ArrayList<>();
+	public Main(){
+		readCourses();
+		
+		Scanner scanner = new Scanner(System.in);
+		int weekDay = scanner.nextInt();
+		System.out.println("請輸入星期(1-7):");
+		int hour = scanner.nextInt();
+		System.out.println("請輸入時(0-24):");
+		
+		
+	}
+	
+	public void readCourses(){
+		
+	}
 	public static void main(String[] args) {
 		
 	}
